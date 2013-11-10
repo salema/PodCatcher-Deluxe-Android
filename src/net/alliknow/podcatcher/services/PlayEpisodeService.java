538a6a9492e87099e679ae1eb1c138d9e60e969a
Kevin Hausmann <kevin@alliknow.net>
2013-11-10 20:55:18 +0100

    Fix issue #104: Store resumeAt information more frequently

--- src/net/alliknow/podcatcher/services/PlayEpisodeService.java
+++ src/net/alliknow/podcatcher/services/PlayEpisodeService.java
@@ -443,6 +443,7 @@ public class PlayEpisodeService extends Service implements MediaPlayerControl,
             Log.d(getClass().getSimpleName(), "Called pause without setting episode");
         else if (prepared && isPlaying()) {
             player.pause();
+            storeResumeAt();
 
             stopPlayProgressTimer();
             updateRemoteControlPlaystate(PLAYSTATE_PAUSED);
