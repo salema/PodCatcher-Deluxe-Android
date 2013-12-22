e47158b4712250f9547701d3d239c898103b528b
Kevin Hausmann <kevin@podcatcher-deluxe.com>
2013-12-22 13:46:01 +0100

    Also use app specific user agent for the player service

--- src/net/alliknow/podcatcher/services/PlayEpisodeService.java
+++ src/net/alliknow/podcatcher/services/PlayEpisodeService.java
@@ -47,6 +47,7 @@ import android.os.PowerManager;
 import android.preference.PreferenceManager;
 import android.util.Log;
 
+import net.alliknow.podcatcher.Podcatcher;
 import net.alliknow.podcatcher.SettingsActivity;
 import net.alliknow.podcatcher.listeners.OnChangePlaylistListener;
 import net.alliknow.podcatcher.listeners.PlayServiceListener;
@@ -287,16 +288,18 @@ public class PlayEpisodeService extends Service implements OnPreparedListener,
                     player.setDataSource(episodeManager.getLocalPath(episode));
                 // Need to resort to remote file
                 else {
-                    // Set source (and authorization if needed)
-                    if (episode.getPodcast().getAuthorization() != null) {
-                        final HashMap<String, String> headers = new HashMap<String, String>(1);
-                        headers.put(AUTHORIZATION_KEY, episode.getPodcast().getAuthorization());
+                    // We add some request headers to overwrite the default user
+                    // agent because this is blocked by some servers
+                    final HashMap<String, String> headers = new HashMap<String, String>(2);
+                    headers.put(Podcatcher.USER_AGENT_KEY, Podcatcher.USER_AGENT_VALUE);
 
-                        player.setDataSource(this, Uri.parse(currentEpisode.getMediaUrl()), headers);
-                    }
-                    // No auth available we could send to the server
-                    else
-                        player.setDataSource(currentEpisode.getMediaUrl());
+                    // Also set the authorization header data if needed
+                    final String auth = episode.getPodcast().getAuthorization();
+                    if (auth != null)
+                        headers.put(AUTHORIZATION_KEY, auth);
+
+                    // Actually set the remote source for the playback
+                    player.setDataSource(this, Uri.parse(currentEpisode.getMediaUrl()), headers);
 
                     // We are streaming, so make wifi stay alive
                     wifiLock.acquire();
