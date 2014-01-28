19c208802bb34155e40b6c8098109103a2eb2a87
Kevin Hausmann <kevin@alliknow.net>
2014-01-28 17:47:54 +0100

    Fix issue #129: Add video "lean back" mode

--- src/net/alliknow/podcatcher/FullscreenVideoActivity.java
+++ src/net/alliknow/podcatcher/FullscreenVideoActivity.java
@@ -17,9 +17,11 @@
 
 package net.alliknow.podcatcher;
 
+import android.annotation.TargetApi;
 import android.content.ComponentName;
 import android.content.Intent;
 import android.content.ServiceConnection;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.IBinder;
@@ -47,8 +49,10 @@ public class FullscreenVideoActivity extends BaseActivity implements VideoSurfac
     /** The media controller overlay */
     private MediaController controller;
 
-    /** Milli-seconds to wait for system ui to be dimmed */
-    private static final int DIM_SYSTEM_UI_DELAY = 1000;
+    /** Milli-seconds to wait for system UI to be hidden */
+    private static final int HIDE_SYSTEM_UI_DELAY = 5000;
+    /** The system UI flags we want to set */
+    private int systemUiFlags;
 
     /** Flag to indicate whether video surface is available */
     private boolean videoSurfaceAvailable = false;
@@ -59,6 +63,7 @@ public class FullscreenVideoActivity extends BaseActivity implements VideoSurfac
     private SurfaceView videoView;
 
     @Override
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
@@ -69,8 +74,16 @@ public class FullscreenVideoActivity extends BaseActivity implements VideoSurfac
             videoView = (SurfaceView) findViewById(R.id.episode_video);
             videoView.getHolder().addCallback(videoCallback);
 
-            // Needed for dimming of the system UI
+            // Needed for dimming/hiding of the system UI
             videoView.setOnSystemUiVisibilityChangeListener(this);
+            // Set system UI flags depending on Android version
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN)
+                systemUiFlags = View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
+            else
+                systemUiFlags |= View.SYSTEM_UI_FLAG_LOW_PROFILE;
 
             // Attach to play episode service
             Intent intent = new Intent(this, PlayEpisodeService.class);
@@ -85,28 +98,28 @@ public class FullscreenVideoActivity extends BaseActivity implements VideoSurfac
     protected void onResume() {
         super.onResume();
 
-        if (controller != null)
-            controller.show();
-
-        videoView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE);
+        videoView.setSystemUiVisibility(systemUiFlags);
+        showMediaControllerOverlay();
     }
 
     @Override
     public void onSystemUiVisibilityChange(int visibility) {
-        if (visibility == View.SYSTEM_UI_FLAG_VISIBLE)
+        if (visibility == View.SYSTEM_UI_FLAG_VISIBLE) {
+            showMediaControllerOverlay();
+
             new Handler().postDelayed(new Runnable() {
 
                 @Override
                 public void run() {
-                    videoView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE);
+                    videoView.setSystemUiVisibility(systemUiFlags);
                 }
-            }, DIM_SYSTEM_UI_DELAY);
+            }, HIDE_SYSTEM_UI_DELAY);
+        }
     }
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
-        if (controller != null)
-            controller.show();
+        showMediaControllerOverlay();
 
         return false;
     }
@@ -152,8 +165,7 @@ public class FullscreenVideoActivity extends BaseActivity implements VideoSurfac
 
     @Override
     public void onPlaybackStateChanged() {
-        if (controller != null)
-            controller.show();
+        showMediaControllerOverlay();
     }
 
     @Override
@@ -203,6 +215,11 @@ public class FullscreenVideoActivity extends BaseActivity implements VideoSurfac
         videoView.setLayoutParams(layoutParams);
     }
 
+    private void showMediaControllerOverlay() {
+        if (controller != null)
+            controller.show();
+    }
+
     /** Defines callbacks for service binding, passed to bindService() */
     private ServiceConnection connection = new ServiceConnection() {
 
@@ -217,6 +234,7 @@ public class FullscreenVideoActivity extends BaseActivity implements VideoSurfac
             controller = new MediaController(FullscreenVideoActivity.this);
             controller.setMediaPlayer(service);
             controller.setAnchorView(videoView);
+
             attachPrevNextListeners();
         }
 
@@ -233,8 +251,7 @@ public class FullscreenVideoActivity extends BaseActivity implements VideoSurfac
         public void surfaceCreated(SurfaceHolder holder) {
             videoSurfaceAvailable = true;
 
-            if (controller != null)
-                controller.show();
+            showMediaControllerOverlay();
         }
 
         @Override
