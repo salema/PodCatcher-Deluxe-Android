f01b408832b6b2d5c673fe91c483f61c3f6508f6
Kevin Hausmann <kevin@podcatcher-deluxe.com>
2014-05-10 18:03:24 +0200

    Version 5.0.3

--- include/gpodder/src/com/dragontek/mygpoclient/Global.java
+++ include/gpodder/src/com/dragontek/mygpoclient/Global.java
@@ -11,6 +11,6 @@ public class Global {
     public static final String WEBSITE = "http://www.podcatcher-deluxe.com";
 
     public static final String USER_AGENT =
-            String.format("VideoPodcatcherDeluxe/5.0.2 (%s)", WEBSITE);
+            String.format("VideoPodcatcherDeluxe/5.0.3 (%s)", WEBSITE);
 
 }
